f1feeeaf6076cba2f5c5b4737d6019c7b0835f7c
Phil Arfuso <arfusop@gmail.com>
2022-11-28 20:03:38 -0500

    client schema

--- gql/resolvers/clients.js
+++ gql/resolvers/clients.js
@@ -13,8 +13,7 @@ const phases = {
     PHASE_2: 'Phase 2: Structure & Design',
     PHASE_3: 'Phase 3: Initial Development',
     PHASE_4: 'Phase 4: Testing/QA',
-    PHASE_5: 'Phase 5: Post Launch Maintenance',
-    PHASE_6: 'Phase 6: New Version Development'
+    PHASE_5: 'Phase 5: Post Launch Maintenance'
 }
 
 module.exports.ClientMutations = {
